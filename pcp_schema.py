321ff7f70e2790eac55e48fa0ed4023cf929b92d
Dustin Black <dblack@redhat.com>
2022-12-08 16:34:38 +0100

    convert schema to annotated typing

--- pcp_schema.py
+++ pcp_schema.py
@@ -1,32 +1,34 @@
 import typing
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+from arcaflow_plugin_sdk import schema
 
 
 @dataclass
 class InputParams:
-    run_duration: int
-    pmlogger_interval: typing.Optional[int] = field(
-        default=None,
-        metadata={
-            "name": "pmlogger logging interval",
-            "description": (
+    run_duration: typing.Annotated[
+        int,
+        schema.name("run duration"),
+        schema.description("Time in seconds that the PCP plugin runs before being forceably stopped"),
+    ]
+    pmlogger_interval: typing.Annotated[
+        typing.Optional[int],
+        schema.name("pmlogger logging interval"),
+        schema.description(
                 "The logging interval in seconds used by "
                 "pmlogger for data collection"
-            ),
-        },
-    )
+        ),
+    ] = None
 
 
 @dataclass
 class PerfOutput:
-    pcp_output: typing.Dict[str, typing.Any] = field(
-        metadata={
-            "name": "PCP output dictionary",
-            "description": (
+    pcp_output: typing.Annotated[
+        typing.Dict[str, typing.Any],
+        schema.name("PCP output dictionary"),
+        schema.description(
                 "Performance data from PCP provided in a " "JSON dictionary format"
-            ),
-        },
-    )
+        ),
+    ]
 
 
 @dataclass
