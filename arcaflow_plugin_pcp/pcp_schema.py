14bc7e97f6cbab28fb7e373b888087c36ffbad3a
Dustin Black <dblack@redhat.com>
2022-12-16 07:17:32 +0100

    Schema update (#3)
    * flatten PCP output to interval list
    
    * fleshed out output schema
    
    * typo fixes

--- arcaflow_plugin_pcp/pcp_schema.py
+++ arcaflow_plugin_pcp/pcp_schema.py
@@ -1,6 +1,6 @@
 import typing
 from dataclasses import dataclass
-from arcaflow_plugin_sdk import schema
+from arcaflow_plugin_sdk import plugin, schema
 
 
 @dataclass
@@ -23,12 +23,78 @@ class InputParams:
 
 
 @dataclass
+class IntervalOutput:
+    interval: typing.Annotated[
+        int,
+        schema.id("@interval"),
+        schema.name("Interval ID"),
+        schema.description("The interval ID as reported by PCP"),
+    ]
+    timestamp: typing.Annotated[
+        str,
+        schema.id("@timestamp"),
+        schema.name("Interval timestamp"),
+        schema.description("The timestamp of the reported interval"),
+    ]
+    commit: typing.Annotated[
+        typing.Optional[typing.Dict[str, typing.Any]],
+        schema.name("commit"),
+        schema.description("The commit for the interval"),
+    ] = None
+    disk: typing.Annotated[
+        typing.Optional[typing.Dict[str, typing.Any]],
+        schema.name("disk"),
+        schema.description("The disk structure for the interval"),
+    ] = None
+    kbin: typing.Annotated[
+        typing.Optional[typing.Dict[str, typing.Any]],
+        schema.name("kbin"),
+        schema.description("The KB in value for the interval"),
+    ] = None
+    kbout: typing.Annotated[
+        typing.Optional[typing.Dict[str, typing.Any]],
+        schema.name("kbout"),
+        schema.description("The KB out value for the interval"),
+    ] = None
+    kernel: typing.Annotated[
+        typing.Optional[typing.Dict[str, typing.Any]],
+        schema.name("kernel"),
+        schema.description("The kernel CPU structure for the interval"),
+    ] = None
+    mem: typing.Annotated[
+        typing.Optional[typing.Dict[str, typing.Any]],
+        schema.name("mem"),
+        schema.description("The memory structure for the interval"),
+    ] = None
+    memused: typing.Annotated[
+        typing.Optional[typing.Dict[str, typing.Any]],
+        schema.name("memused"),
+        schema.description("The memory used value for the interval"),
+    ] = None
+    pktin: typing.Annotated[
+        typing.Optional[typing.Dict[str, typing.Any]],
+        schema.name("pktin"),
+        schema.description("The packets in for the interval"),
+    ] = None
+    pktout: typing.Annotated[
+        typing.Optional[typing.Dict[str, typing.Any]],
+        schema.name("pktout"),
+        schema.description("The packets out for the interval"),
+    ] = None
+
+
+interval_output_schema = schema.ListType(
+    plugin.build_object_schema(IntervalOutput)
+)
+
+
+@dataclass
 class PerfOutput:
     pcp_output: typing.Annotated[
-        typing.Dict[str, typing.Any],
-        schema.name("PCP output dictionary"),
+        typing.List[IntervalOutput],
+        schema.name("PCP output list"),
         schema.description(
-            "Performance data from PCP provided in a " "JSON dictionary format"
+            "Performance data from PCP provided in a list format"
         ),
     ]
 
